ee00baf27dd23b732b045cdf00a11d20f4dfdbd9
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2018-08-05 20:44:58 +0200

    First working version, with 100% tests coverage

---
+++ __tests__/duplex.js
@@ -0,0 +1,32 @@
+const {PassThrough} = require('stream')
+
+const {duplex} = require('..')
+
+
+test('object', function(done)
+{
+  const expected = {asdf: 1234}
+
+  duplex(new PassThrough)
+  .on('data', function(data)
+  {
+    expect(data).toEqual(expected)
+
+    done()
+  })
+  .write(expected)
+})
+
+test('string', function(done)
+{
+  const expected = "asdf"
+
+  duplex(new PassThrough)
+  .on('data', function(data)
+  {
+    expect(data.toString()).toEqual(expected)
+
+    done()
+  })
+  .write(expected)
+})
